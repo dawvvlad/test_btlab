14f0d4513ab9dff524d73ae6b56bca27a5669d8e
dawvvlad <vgolikov1998@gmail.com>
2025-01-18 18:33:52 +0300

    update

--- src/main/java/org/golikov/test_app/controllers/ApiTimeSheetController.java
+++ src/main/java/org/golikov/test_app/controllers/ApiTimeSheetController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+/**
+* REST-контроллер для управления таблицей опозданий
+**/
 @RestController
 @RequestMapping("api/v1/timesheet")
 public class ApiTimeSheetController {
@@ -24,6 +27,11 @@ public class ApiTimeSheetController {
         this.timesheetService = timesheetService;
     }
 
+    /**
+     * Создание нового опоздания
+     * @param timesheetCreateRequest запрос для создания записи
+     * @throws NoSuchValueException в случае неверных данных выбрасывает исключение и возвращает строку с сообщением
+     */
     @PostMapping
     public ResponseEntity<TimesheetCreateRequest> createTimesheet(@RequestBody TimesheetCreateRequest timesheetCreateRequest) {
         try {
@@ -33,6 +41,8 @@ public class ApiTimeSheetController {
             throw new NoSuchValueException("Incorrect data (Employee with id " + timesheetCreateRequest.getEmployeeId() + " not found)");
         }
     }
+
+
 
     @GetMapping
     public ResponseEntity<List<TimesheetDTO>> getTimesheets() {
@@ -55,22 +65,36 @@ public class ApiTimeSheetController {
     public ResponseEntity<Void> deleteTimesheetById(@PathVariable Long id) {
         timesheetService.delete(id);
         System.out.println("deleted timesheet");
+        if(timesheetService.getById(id) == null) {
+            throw new NoSuchValueException("Timesheet with id " + id + " not found");
+        }
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+
+    /**
+     * Создание нового опоздания
+     * @param id id записи для изменения
+     * @param updates объект, представляющий собой поле и данные для изменения
+     * @throws NoSuchValueException в случае неверных данных выбрасывает исключение и возвращает строку с сообщением
+     */
     @PatchMapping("/{id}")
     public ResponseEntity<TimesheetDTO> updateTimesheetById(
             @PathVariable Long id,
             @RequestBody Map<String, Object> updates
     ) {
 
+        // находится запись по id
         TimesheetDTO existingTimesheet = timesheetService.getById(id);
 
         if (existingTimesheet == null) {
             throw new NoSuchValueException("Timesheet with id " + id + " not found");
         }
 
+        // создается объект запроса (для удобства передачи Employee в виде id)
         TimesheetCreateRequest timesheetCreateRequest = new TimesheetCreateRequest(existingTimesheet);
+
+        // проверка на соответствие ключа в Map и поля в таблице
         updates.forEach((key, value) -> {
             switch (key) {
                 case "reason":
@@ -95,6 +119,7 @@ public class ApiTimeSheetController {
                     throw new IllegalArgumentException("Unknown field: " + key);
             }
         });
+
 
         TimesheetDTO updatedTimesheet = timesheetService.update(timesheetCreateRequest);
         return ResponseEntity.status(HttpStatus.OK).body(updatedTimesheet);
